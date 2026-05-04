2b2690795339d4dd0f56d134ae9ea2f66c2f279d
Dhanashree <dhanashreeakharade@gmail.com>
2026-05-04 16:43:59 -0400

    created simple calculator

--- OOPs/Classes/calc.java
+++ OOPs/Classes/calc.java
@@ -2,23 +2,40 @@ package OOPs.Classes;
 
 public class calc {
     public static void main(String[] args) {
-        Calculator calc = new Calculator();
-        System.out.println(calc.sum(1, 2));
-        System.out.println(calc.sum((float) 1.5, (float) 2.5));
-        System.out.println(calc.sum(1, 2, 5));
+
+        Deer d = new Deer();
+        d.eat();
+        // Calculator calc = new Calculator();
+        // System.out.println(calc.sum(1, 2));
+        // System.out.println(calc.sum((float) 1.5, (float) 2.5));
+        // System.out.println(calc.sum(1, 2, 5));
     }
 }
 
-class Calculator {
-    int sum(int a, int b) {
-        return a + b;
+class Animal {
+    void eat() {
+        System.out.println("Eats grass");
     }
 
-    float sum(float a, float b) {
-        return a + b;
-    }
-
-    int sum(int a, int b, int c) {
-        return a + b + c;
-    }
 }
+
+class Deer extends Animal {
+    void eat() {
+        System.out.println("Eats Grass");
+    }
+
+}
+
+// class Calculator {
+// int sum(int a, int b) {
+// return a + b;
+// }
+
+// float sum(float a, float b) {
+// return a + b;
+// }
+
+// int sum(int a, int b, int c) {
+// return a + b + c;
+// }
+// }
