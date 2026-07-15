cde26471b589dcdadee84e0f39e503a3143f41ed
Dhanashree <dhanashreeakharade@gmail.com>
2026-07-15 05:14:24 -0400

    find any pair in sorted arraylist has a target sum

---
+++ ArrayList/pairSum.java
@@ -0,0 +1,34 @@
+//find any pair in sorted arraylist has a target sum
+package ArrayList;
+
+import java.util.ArrayList;
+
+public class pairSum {
+
+    public static boolean pairSum1(ArrayList<Integer> list, int target) {
+        for (int i = 0; i < list.size(); i++) {
+            for (int j = i + 1; j < list.size(); j++) {
+                if (list.get(i) + list.get(j) == target) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static void main(String args[]) {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        // 1,2,3,4,5,6
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+        int target = 50;
+
+        System.out.println(pairSum1(list, target));
+    }
+
+}
