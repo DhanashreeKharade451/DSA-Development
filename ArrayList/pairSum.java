a244cfe7f2f31ad70407501545d8efe050251363
Dhanashree <dhanashreeakharade@gmail.com>
2026-07-15 05:53:07 -0400

    find any pair in sorted arraylist has a target sum 2 pointer approach

--- ArrayList/pairSum.java
+++ ArrayList/pairSum.java
@@ -1,18 +1,43 @@
 //find any pair in sorted arraylist has a target sum
+
 package ArrayList;
 
 import java.util.ArrayList;
 
 public class pairSum {
 
+    // //Brute force approach find all possible solutions T.C = O(n^2)
+    // public static boolean pairSum1(ArrayList<Integer> list, int target) {
+    // for (int i = 0; i < list.size(); i++) {
+    // for (int j = i + 1; j < list.size(); j++) {
+    // if (list.get(i) + list.get(j) == target) {
+    // return true;
+    // }
+    // }
+    // }
+    // return false;
+    // }
+
+    // 2 pointer approach
     public static boolean pairSum1(ArrayList<Integer> list, int target) {
-        for (int i = 0; i < list.size(); i++) {
-            for (int j = i + 1; j < list.size(); j++) {
-                if (list.get(i) + list.get(j) == target) {
-                    return true;
-                }
+        int lp = 0;
+        int rp = list.size() - 1;
+
+        while (lp != rp) {
+            // case1
+            if (list.get(lp) + list.get(rp) == target) {
+                return true;
             }
+
+            // case2
+            if (list.get(lp) + list.get(rp) < target) {
+                lp++;
+            } else {
+                rp--;
+            }
+
         }
+
         return false;
     }
 
@@ -26,7 +51,7 @@ public class pairSum {
         list.add(4);
         list.add(5);
         list.add(6);
-        int target = 50;
+        int target = 5;
 
         System.out.println(pairSum1(list, target));
     }
